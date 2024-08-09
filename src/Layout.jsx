8dd0fdc0ccf5a932b34e25740b62297bf94c8077
Anubhav Tripathi <anubhavtripathi0509@gmail.com>
2024-08-09 14:21:43 +0530

    commit

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 
 const Layout = ({ children }) => {
   return (
+    <>
     <div className="flex">
       <Sidebar />
       <div className="flex-grow p-4 bg-blue-50 min-h-screen">
@@ -12,6 +13,12 @@ const Layout = ({ children }) => {
         <main className="p-4">{children}</main>
       </div>
     </div>
+    <footer className="footer footer-center bg-base-300 text-base-content p-4">
+      <aside>
+        <p>Copyright © {new Date().getFullYear()} - All right reserved by Awrim Technologies, Developed by Anubhav Tripathi: 8104083562</p>
+      </aside>
+    </footer>
+    </>
   );
 };
 
